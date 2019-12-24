b71d1e362a7a455c512ff6f367f7c5f83666d285
tsir-mac <841227297@qq.com>
2019-12-24 15:54:12 +0800

    asyncio

---
+++ crawler/req.py
@@ -0,0 +1,98 @@
+# coding=utf-8
+
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent': 'Chrome/45.0.2454.85 Safari/537.36 115Browser/6.0.3'}
+
+
+# 实习僧主页
+def get_sxs_index():
+    url = 'https://www.shixiseng.com/'
+    response = requests.get(url)
+    with open('test.html', 'w+') as f:
+        f.write(response.text)
+
+
+# 登陆实习僧
+def login_sxs():
+    login_url = 'https://www.shixiseng.com/user/login'
+    req = requests.Session()
+    param = {
+        'username': '18728263016',
+        'password': '45X35X25X15X05X94',
+        'remember_login': '1'
+    }
+
+    resp = req.post(login_url, headers=headers, data=param)
+    print(resp.text)
+    return req.cookies
+
+
+def login_sxs_sit1():
+    login_url = 'http://sit1-sxs-web.mshare.cn/user/login'
+    req = requests.Session()
+    param = {
+        'username': '15196801320',
+        'password': '45X35X25X15X05X94',
+        'remember_login': '1'
+    }
+
+    resp = req.post(login_url, headers=headers, data=param)
+    print(resp.text)
+    return req.cookies
+
+
+# 我的投递
+def get_delivers():
+    c = login_sxs_sit1()
+    url2 = 'http://sit1-sxs-web.mshare.cn/my/delivered'
+    response2 = requests.get(url2, headers=headers, cookies=c)
+    # response2 = requests.get(url2, headers=headers)
+    with open('test2.html', 'w+') as f:
+        f.write(response2.text)
+
+
+# 首页职位
+def get_index_interns():
+    url = 'https://www.shixiseng.com/'
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, features='lxml')
+    # print(soup.prettify())
+    interns = soup.find_all('div', {"class": "intern-content-href"})
+    for intern in interns:
+        # print(intern)
+
+        tags = intern.find_all('div', {"class": "job-tags-text"})
+        tags = [tag.text for tag in tags if tag.text != '···']
+        img = intern.find('img').attrs.get('src')
+
+        a_tags = intern.find_all('a')
+        name = a_tags[0].text
+        company = a_tags[2].text
+
+        spans = intern.find_all('span')
+        salary = spans[0].text
+        city = spans[1].text
+        work_day = spans[2].text
+        month = spans[3].text
+        industry = spans[4].text
+        scale = spans[5].text.replace('丨', '')
+        print(name, salary, tags, city, work_day, month, img, company, industry, scale)
+
+
+def intern_search():
+    url = ''
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, features='lxml')
+    interns = soup.find_all('div', {'class': 'intern-wrap intern-item'})
+    print(interns)
+
+
+if __name__ == '__main__':
+    # get_sxs_index()
+    # get_index_interns()
+    get_delivers()
+    # intern_search()
---
+++ crawler/selenium_js.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+cookies = {}
+
+
+def init_browser():
+
+    path = "/Users/tsir/study/code/crawler/chromedriver"  # 注意这个路径需要时可执行路径（chmod 777 dir or 755 dir）
+
+    options = webdriver.ChromeOptions()
+
+    options.add_argument('--headless')  # 无界面模式
+    options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+    options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
+    options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
+    # options.add_argument("--proxy-server=http://202.20.16.82:10152")  # 设置ip代理
+    options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"  # 手动指定使用的浏览器位置
+
+    browser = webdriver.Chrome(options=options, executable_path=path)
+    browser.implicitly_wait(10)  # 定位元素时的等待时间
+    return browser
+
+
+def demo(browser):
+
+    browser.get('www.baidu.com')
+    print(browser.page_source)
+    browser.save_screenshot('baidu.png')  # 截图
+
+
+def login(browser):
+    browser.get('http://sit1-sxs-web.mshare.cn/user/login')
+    account = browser.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div[1]/input')
+    password = browser.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div[2]/input')
+    account.send_keys('15196801320')
+    password.send_keys('123456')
+    browser.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]').click()
+    browser.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/ul/li[2]/a')
+    global cookies
+    if not cookies:
+        cookies = browser.get_cookies()
+
+
+def my_deliver(browser):
+    for cookie in cookies:
+        if 'expiry' in cookie:
+            del cookie['expiry']
+        browser.add_cookie(cookie)
+    browser.get('http://sit1-sxs-web.mshare.cn/my/delivered')
+    browser.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div/div[2]/div[1]/div[1]')
+    with open('test3.html', 'w+') as f:
+        f.write(browser.page_source)
+
+    # bs4 直接解析browser.page_source
+
+
+if __name__ == '__main__':
+    browser = init_browser()
+    login(browser)
+    my_deliver(browser)
+
+    browser.close()  # 记得关闭浏览器节省资源
